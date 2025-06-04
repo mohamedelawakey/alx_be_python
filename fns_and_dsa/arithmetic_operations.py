985bb14ac4c864797b4f3c3b51b22038b0cf021c
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-06-04 17:30:40 +0300

    edit task 1 for divide operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,5 +11,4 @@ def perform_operation(num1, num2, operation):
             if num2 == 0:
                 return "Error: Division by zero"
             else:
-                return num1 + num2
-        
+                return num1 / num2
