2f63a476233fea49abc3ae8ab42ea21fc0155d0c
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-06-05 02:36:00 +0300

    task 4 edited for c and f of degrees

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,19 +1,19 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR, CELSIUS_TO_FAHRENHEIT_FACTOR = 5/9, 9/5
 
 def convert_to_celsius(fahrenheit):
-    to_celsius = (temperature - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(f'{temperature}°F is {to_celsius}°C')
+    to_celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f'{fahrenheit}°F is {to_celsius}°C')
 
 def convert_to_fahrenheit(celsius):
-    to_fahrenheit = (temperature * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-    print(f'{temperature}°C is {to_fahrenheit}°F')
+    to_fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f'{celsius}°C is {to_fahrenheit}°F')
 
 temperature = float(input('Enter the temperature to convert: '))
-Celsius_or_Fahrenheit = input('Is this temperature in Celsius or Fahrenheit? (C/F): ')
+Celsius_or_Fahrenheit = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').strip().upper()
 
 if Celsius_or_Fahrenheit == 'F':
     convert_to_celsius(temperature)
 elif Celsius_or_Fahrenheit == 'C':
     convert_to_fahrenheit(temperature)
 else:
-    print('erroe')
+    print('Error: Please enter "C" for Celsius or "F" for Fahrenheit.')
