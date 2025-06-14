d5d532a0872c600786f425b015dc1afc6f77aa0b
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-06-14 22:43:46 +0300

    edit main 2 ..

--- programming_paradigm/main2.py
+++ programming_paradigm/main2.py
@@ -9,7 +9,7 @@ def main():
     # Initial list of available books
     print("Available books after setup:")
     library.list_available_books()
-
+ 
     # Simulate checking out a book
     library.check_out_book("1984")
     print("\nAvailable books after checking out '1984':")
